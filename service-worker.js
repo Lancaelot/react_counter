72a426bc12a97cd88467bb955fd2744b47d6fcf1
Lancaelot <Lancaelot@gmail.com>
2020-10-29 09:33:33 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.4a4ef4c95fb891dc76fb1521693a969e.js"
 );
 
 self.addEventListener('message', (event) => {
